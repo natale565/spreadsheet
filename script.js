30f7d2bd582e57c73f03faac472f46827e4250c1
Chris Natale <natale565@icloud.com>
2025-01-20 20:28:21 -0500

    spreadsheet functions added to JS

--- script.js
+++ script.js
@@ -10,21 +10,38 @@ const infixEval = (str, regex) =>
     infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
   );
 
-  const highPrecedence = (str) => {
-    const regex = /([\d.]+)([*\/])([\d.]+)/;
-    const str2 = infixEval(str, regex);
-    return str === str2 ? str : highPrecedence(str2);
-  };
+const highPrecedence = (str) => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
+};
 
-  const isEven = (num) => num % 2 === 0;
-  const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
-  const average = (nums) => sum(nums) / nums.length;
+const isEven = (num) => num % 2 === 0;
+const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
+const average = (nums) => sum(nums) / nums.length;
 
-  const median = (nums) => {
-    const sorted = nums.slice().sort((a, b) => a - b);
-    const length = sorted.length;
-    const middle = length / 2 - 1;
-    return isEven(length)
-      ? average([sorted[middle], sorted[middle + 1]])
-      : sorted[Math.ceil(middle)];
-  };
+const median = (nums) => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+};
+
+const spreadsheetFunctions = {
+  "": (nums) => nums,
+  sum,
+  average,
+  median,
+  even: (nums) => nums.filter(isEven),
+  someeven: (nums) => nums.some(isEven),
+  everyeven: (nums) => nums.every(isEven),
+  firsttwo: (nums) => nums.slice(0, 2),
+  lasttwo: (nums) => nums.slice(-2),
+  has2: (nums) => nums.includes(2),
+  increment: (nums) => nums.map((num) => num + 1),
+  random: ([x, y]) => Math.floor(Math.random() * y + x),
+  range: (nums) => range(...nums),
+  nodupes: (nums) => [...new Set(nums).values()],
+};
