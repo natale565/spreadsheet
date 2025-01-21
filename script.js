7d9333f1d9c7ed7e834917423111358546718872
Chris Natale <natale565@icloud.com>
2025-01-20 20:27:00 -0500

    updates to js

--- script.js
+++ script.js
@@ -15,3 +15,7 @@ const infixEval = (str, regex) =>
     const str2 = infixEval(str, regex);
     return str === str2 ? str : highPrecedence(str2);
   };
+
+  const isEven = (num) => num % 2 === 0;
+  const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
+  const average = (nums) => sum(nums) / nums.length;
