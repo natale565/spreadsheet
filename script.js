2a7420a56418150973536c5b471b19a61dac39c8
Chris Natale <natale565@icloud.com>
2025-01-20 20:31:26 -0500

    update to js

--- script.js
+++ script.js
@@ -92,3 +92,26 @@ const charRange = (start, end) =>
       ? functionExpanded
       : evalFormula(functionExpanded, cells);
   };
+
+  window.onload = () => {
+    const container = document.getElementById("container");
+    const createLabel = (name) => {
+      const label = document.createElement("div");
+      label.className = "label";
+      label.textContent = name;
+      container.appendChild(label);
+    };
+    const letters = charRange("A", "J");
+    letters.forEach(createLabel);
+    range(1, 99).forEach((number) => {
+      createLabel(number);
+      letters.forEach((letter) => {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.id = letter + number;
+        input.ariaLabel = letter + number;
+        input.onchange = update;
+        container.appendChild(input);
+      });
+    });
+  };
