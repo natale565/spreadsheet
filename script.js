aa18adc6176029129cd205213eb99cb1f797a0f8
Chris Natale <natale565@icloud.com>
2025-01-20 20:25:27 -0500

    high precedence function added

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ const infixEval = (str, regex) =>
   str.replace(regex, (_match, arg1, operator, arg2) =>
     infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
   );
+
+  const highPrecedence = (str) => {
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
+    const str2 = infixEval(str, regex);
+    return str === str2 ? str : highPrecedence(str2);
+  };
